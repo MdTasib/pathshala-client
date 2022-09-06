0546befd885f51cc9932a069191dd66372dee968
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-09-07 05:22:42 +0600

    our classes

--- src/pages/Home/components/OurClass/OurClass.jsx
+++ src/pages/Home/components/OurClass/OurClass.jsx
@@ -1,176 +1,50 @@
-import React from 'react';
-import "./OurClass.css"
-import InfiniteCarousel from 'react-leaf-carousel';
-import img1 from "../../../../assets/images/1.jpg"
+import React from "react";
+import "./OurClass.css";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import img1 from "../../../../assets/images/classimg1.jpg";
+import SingleCardClass from "./SingleCardClass";
 const OurClass = () => {
-    return (
-        <section className='min-h-screen relative'>
-            <div className='container'>
-                <div className='section-title'>
-                    <h2 className='section-heading '>Our Classes</h2>
-                    <p>Nam mattis felis id sodales rutrum. Nulla ornare tristique mauris, a laoreet erat ornare sit amet. Nulla sagittis faucibus lacus</p>
-                </div>
-                {/* section title end */}
+    const items = [1,2,3,4,5,1,2,3,4,5]
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 1560 },
+      items: 4,
+    },
+    desktop: {
+      breakpoint: { max: 1560, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 780 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 780, min: 0 },
+      items: 1,
+    },
+  };
+  return (
+    <section className=" relative pt-10">
+      <div className="container">
+        <div className="section-title">
+          <h2 className="section-heading ">Our Classes</h2>
+          <p>
+            Nam mattis felis id sodales rutrum. Nulla ornare tristique mauris, a
+            laoreet erat ornare sit amet. Nulla sagittis faucibus lacus
+          </p>
+        </div>
+        {/* section title end */}
 
-                <div className='class-section'>
-                    <InfiniteCarousel
-                                    breakpoints={[
-                                    {
-                                        breakpoint: 500,
-                                        settings: {
-                                        slidesToShow: 2,
-                                        slidesToScroll: 2,
-                                        },
-                                    },
-                                    {
-                                        breakpoint: 768,    
-                                        settings: {
-                                        slidesToShow: 3,
-                                        slidesToScroll: 3,
-                                        },
-                                    },
-                                    ]}
-                                        dots={true}
-                                        showSides={true}
-                                        sidesOpacity={.5}
-                                        sideSize={.1}
-                                        slidesToScroll={4}
-                                        slidesToShow={4}
-                                        scrollOnDevice={true}
-                                    >
-                                        <div className='container' >
-                                        <img className='rounded-lg h-56'
-                                            alt=''
-                                            src={img1}
-                                        />
-                                        <p className='text-2xl'>asdfjoh</p>
-                                        </div>
-                                        <div>
-                                        <img
-                                            alt=''
-                                            src={img1}
-                                        />
-                                        </div>
-                                        <div>
-                                        <img
-                                            alt=''
-                                            src={img1}
-                                        />
-                                        </div>
-                                        <div>
-                                        <img
-                                            alt=''
-                                            src={img1}
-                                        />
-                                        </div>
-                                        <div>
-                                        <img
-                                            alt=''
-                                            src={img1}
-                                        />
-                                        </div>
-                                        <div>
-                                        <img
-                                            alt=''
-                                            src={img1}
-                                        />
-                                        </div>
-                                        <div>
-                                        <img
-                                            alt=''
-                                            src={img1}
-                                        />
-                                        </div>
-                                        <div>
-                                        <img
-                                            alt=''
-                                            src={img1}
-                                        />
-                                        </div>
-                                        <div>
-                                        <img
-                                            alt=''
-                                            src={img1}
-                                        />
-                                        </div>
-                                        <div>
-                                        <img
-                                            alt=''
-                                            src={img1}
-                                        />
-                                        </div>
-                                        <div>
-                                        <img
-                                            alt=''
-                                            src={img1}
-                                        />
-                                        </div>
-                                        <div>
-                                        <img
-                                            alt=''
-                                            src={img1}
-                                        />
-                                        </div>
-                                        <div>
-                                        <img
-                                            alt=''
-                                            src={img1}
-                                        />
-                                        </div>
-                                        <div>
-                                        <img
-                                            alt=''
-                                            src={img1}
-                                        />
-                                        </div>
-                                        <div>
-                                        <img
-                                            alt=''
-                                            src={img1}
-                                        />
-                                        </div>
-                                        <div>
-                                        <img
-                                            alt=''
-                                            src={img1}
-                                        />
-                                        </div>
-                                        <div>
-                                        <img
-                                            alt=''
-                                            src={img1}
-                                        />
-                                        </div>
-                                        <div>
-                                        <img
-                                            alt=''
-                                            src={img1}
-                                        />
-                                        </div>
-                                        <div>
-                                        <img
-                                            alt=''
-                                            src={img1}
-                                        />
-                                        </div>
-                                        <div>
-                                        <img
-                                            alt=''
-                                            src={img1}
-                                        />
-                                        </div>
-                    </InfiniteCarousel>
-
-                </div>
-
-
-
-
-
-            </div>
-
-        </section>
-    );
+        <div className="class-section ">
+          <Carousel responsive={responsive}>
+            {items.map(item =><SingleCardClass img1={img1}></SingleCardClass>)}
+            </Carousel>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default OurClass;
